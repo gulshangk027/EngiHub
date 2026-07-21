a2200997df24fbe17bc648bc9e6bec5d5404c411
gulshangk027 <gulshankumargk0023@gmail.com>
2026-07-22 02:49:45 +0530

    Add Vercel entrypoint config

---
+++ api/index.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Add root folder to sys.path so 'app' can be imported correctly by Vercel
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import create_app
+
+app = create_app()
